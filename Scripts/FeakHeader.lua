2c11301b8cb67e98c07337daf99d1bf22be1c7e6
AXiX-official <2879710747@qq.com>
2024-07-26 10:32:06 +0800

    Update FakeHeader.lua

--- Scripts/FeakHeader.lua
+++ Scripts/FeakHeader.lua
@@ -3,19 +3,57 @@
 -- AssetStudio-Version: 1.37.04
 -- Description: This script is used to remove the fake header of the file.
 
-function get_offset(stream)
-    local readr = CreateEndianBinaryReader(stream)
-    local buffer = readr:ReadBytes(1024)
-    local firstSevenChars = string.char(table.unpack(buffer, 0, 6))
-    local firstPos = 0
-    if firstSevenChars == 'UnityFS' then
-        firstPos = 7
-    elseif buffer[1] == 0x89 then
-        firstPos = 1
+local max_length = 2048
+
+function read_to_null(buffer, index)
+    -- 读取到'00'为止
+    local i = index
+    while buffer[i] ~= 0 do
+        i = i + 1
     end
-    local allText = string.char(table.unpack(buffer, 0 , 1023))
-    local secondPos = string.find(allText, 'UnityFS', firstPos)
-    return secondPos - 1
+    return i + 1
+end
+
+function get_size_from_header(buffer, index)
+    local i = index + 11
+    i = read_to_null(buffer, i)
+    i = read_to_null(buffer, i)
+    local size = 0
+    for j = 1, 8 do
+        size = size * 256 + buffer[i + j - 1]
+    end
+    return size
+end
+
+function max(a, b)
+    if a > b then
+        return a
+    else
+        return b
+    end
+end
+
+function get_offset(stream)
+    local length = stream.Length
+    Verbose("File length: " .. length)
+    local readr = CreateEndianBinaryReader(stream)
+    local buffer = readr:ReadBytes(max_length)
+    local allText = string.char(table.unpack(buffer, 0 , max_length - 1))
+    local indexs = {}
+    local i = string.find(allText, 'UnityFS', 0)
+    while i do
+        Verbose("Find UnityFS at " .. i)
+        table.insert(indexs, i)
+        i = string.find(allText, 'UnityFS', i + 1)
+    end
+    for j = #indexs, 1, -1 do
+        local j_size = get_size_from_header(buffer, indexs[j])
+        Verbose("UnityFS size: " .. j_size)
+        if j_size + indexs[j] - 1 == length then
+            return indexs[j]
+        end
+    end
+    return -1
 end
     
 SetGame("Normal")
